06e997c55de763a7eccb795af5abbacbee11d098
pityka <gordonfreeman@freemail.hu>
2012-02-20 18:24:52 +0100

    project local repository with play RC3 and pityu commons

--- project/Build.scala
+++ project/Build.scala
@@ -14,11 +14,12 @@ object ApplicationBuild extends Build {
 
   val scalatest = "org.scalatest" %% "scalatest_2.9.0" % "1.6" % "test" from "http://www.scala-tools.org/repo-snapshots/org/scalatest/scalatest_2.9.0/1.6-SNAPSHOT/scalatest_2.9.0-1.6-SNAPSHOT.jar"
 
-    val localResolver = Resolver.file("Local ivy ~/.ivy2/local", file("/Users/pityka/.ivy2/local"))(Resolver.ivyStylePatterns)
+   
+
+
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here    
-      resolvers += localResolver
   
     ) dependsOn (genesClusters)
 
@@ -26,8 +27,7 @@ object ApplicationBuild extends Build {
     lazy val genesClusters = Project(
     id = "genes_clusters",
     base = file("genes_clusters/"),
-    settings = Defaults.defaultSettings ++ Seq (libraryDependencies ++= appDependencies ++ Seq(scalatest),
-      resolvers += localResolver)
+    settings = Defaults.defaultSettings ++ Seq (libraryDependencies ++= appDependencies ++ Seq(scalatest))
     )
 
 }
