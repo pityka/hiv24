5eb88226fc37ae7fbe965dc463a23ba9dbd83678
Istvan Bartha <bartha.pityu@gmail.com>
2012-09-20 18:58:09 +0200

    update datafilee from pejman

--- project/Build.scala
+++ project/Build.scala
@@ -11,9 +11,6 @@ object ApplicationBuild extends Build {
       // Add your project dependencies here,
       "pityu" %% "commons" % "0.1"      
     )
-
-  val scalatest = "org.scalatest" %% "scalatest_2.9.0" % "1.6" % "test" from "http://www.scala-tools.org/repo-snapshots/org/scalatest/scalatest_2.9.0/1.6-SNAPSHOT/scalatest_2.9.0-1.6-SNAPSHOT.jar"
-
    
 
 
@@ -27,7 +24,7 @@ object ApplicationBuild extends Build {
     lazy val genesClusters = Project(
     id = "genes_clusters",
     base = file("genes_clusters/"),
-    settings = Defaults.defaultSettings ++ Seq (libraryDependencies ++= appDependencies ++ Seq(scalatest))
+    settings = Defaults.defaultSettings ++ Seq (libraryDependencies ++= appDependencies ++ Seq())
     )
 
 }
